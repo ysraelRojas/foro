dd2d8c3a5adedbcd4a357e760585902c6556471a
Ysrael Rojas Rojas <yrra_rojas@hotmail.com>
2017-10-02 00:27:12 -0500

    Validacion de datos de un formulario con TDD

--- tests/features/CreatePostsTest.php
+++ tests/features/CreatePostsTest.php
@@ -27,6 +27,16 @@ class CreatePostsTest extends FeatureTestCase
 			 ->seePageIs(route('login'));
 	}
 
+	public function test_create_post_form_validation()
+	{
+		$this->actingAs($this->defaultUser())
+			 ->visit(route('posts.create'))
+			 ->press('Publicar')
+			 ->seePageIs(route('posts.create'))
+			 ->seeInElement('#field_title.has-error .help-block', 'El campo título es obligatorio')
+			 ->seeInElement('#field_content.has-error .help-block', 'El campo contenido es obligatorio');
+	}
+
 }
 
  ?>
